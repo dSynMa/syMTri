1f92d82eefd7892838c78cf91a13459d18c0f5b8
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2021-05-07 11:28:27 +0200

    input parameters for doing nuxmv translation

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from synthesis.synthesis import synthesize_seq_rep, synthesize_seq
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--m', dest='monitor', type=str)
+    parser.add_argument('--to_nuxmv', dest='to_nuxmv', type=bool, nargs='?', const=True, default=False)
+    parser.add_argument('--to_nuxmv_cases', dest='to_nuxmv_cases', type=bool, nargs='?', const=True, default=False)
     parser.add_argument('--l', dest='ltl', type=str)
     parser.add_argument('--rep', dest='rep', type=bool, nargs='?', const=True, default=False)
     parser.add_argument('--server', dest='server', type=str)
@@ -16,12 +18,23 @@ def main():
 
     if args.monitor is None:
         raise Exception("Monitor path not specified.")
-    if args.ltl is None:
-        raise Exception("TLSF path not specified.")
 
     date_file = open(args.monitor, "r").read()
 
     date = string_to_monitor(date_file)
+
+    if args.to_nuxmv:
+        print(date.to_nuXmv())
+    else:
+        print(date.to_nuXmv_case_style())
+
+    if args.ltl is None:
+        if not args.to_nuxmv and not args.to_nuxmv_cases:
+            raise Exception("TLSF path not specified.")
+        else:
+            return
+
+
     if args.rep:
         (realiz, date) = synthesize_seq_rep(date, args.ltl, args.server, args.docker)
     else:
