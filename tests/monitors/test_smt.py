fff914b9f72f9158d0af1174c5e00c08b148bfa6
Luca Di Stefano <lucad@chalmers.se>
2022-07-18 16:37:08 +0200

    Add some LTL-to-SMT tests

--- tests/monitors/test_smt.py
+++ tests/monitors/test_smt.py
@@ -1,30 +1,76 @@
+import os
+from subprocess import check_output
 from unittest import SkipTest, TestCase
 
-from pysmt.shortcuts import Symbol, get_env, Solver
+from prop_lang.parsing.string_to_ltl import string_to_ltl
+from pysmt.factory import SolverRedefinitionError
 from pysmt.logics import QF_UFLRA
-from subprocess import check_output
-import os
+from pysmt.shortcuts import Solver, Symbol, get_env
+
+from monitors.typed_valuation import TypedValuation
 
 
 class TestSmt(TestCase):
 
+    SOLVER_NAME = "mathsat-smtlib"
+
+    def _add_mathsat(self):
+        self._check_os()
+
+        which_cmd = "which" if os.name == "posix" else "where"
+        msat_path = check_output((which_cmd, "mathsat"), encoding="utf8")
+        msat_path = msat_path.replace("\n", "")
+        # Add the solver to the environment
+        env = get_env()
+        try:
+            env.factory.add_generic_solver(self.SOLVER_NAME, [msat_path], [QF_UFLRA])
+        except SolverRedefinitionError:
+            # Solver has already been registered, skip
+            pass
+
+    def _check_os(self):
+        if os.name not in ("posix", "nt"):
+            raise SkipTest(f"This test does not support OS '{os.name}'.")
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self._add_mathsat()
+
     def test_mathsat(self):
         """Tests that a system-wide mathsat can be found and driven by pysmt
         """
-
-        if os.name == "posix":
-            which_cmd = "which"
-        elif os.name == "nt":
-            which_cmd = "where"
-        else:
-            raise SkipTest(f"This test does not support OS '{os.name}'.")
-
-        msat_path = check_output((which_cmd, "mathsat"), encoding="utf8")
-        msat_path = msat_path.replace("\n", "")
-        name = "mathsat-smtlib"
-
-        # Add the solver to the environment
-        env = get_env()
-        env.factory.add_generic_solver(name, [msat_path], [QF_UFLRA])
-        with Solver(name=name) as s:
+        self._check_os()
+        with Solver(name=self.SOLVER_NAME) as s:
             self.assertTrue(s.is_sat(Symbol("x")))
+
+    def _test_to_smt(self, ltl_str: str, symbols: dict, expected_sat: bool):
+        self._check_os()
+        ltl = string_to_ltl(ltl_str)
+        smt = ltl.to_smt(symbols)
+        with Solver(name=self.SOLVER_NAME) as s:
+            self.assertEqual(s.is_sat(smt), expected_sat)
+
+    def test_to_smt_0(self):
+        self._test_to_smt("1 == 0", {}, False,)
+
+    def test_to_smt_1(self):
+        self._test_to_smt(
+            "TO_BE || (!TO_BE)",
+            {"TO_BE": TypedValuation("TO_BE", "bool", True)},
+            True)
+
+    def test_to_smt_2(self):
+        self._test_to_smt(
+            "TO_BE && (!TO_BE)",
+            {"TO_BE": TypedValuation("TO_BE", "bool", True)},
+            False)
+
+    def test_to_smt_3(self):
+        self._test_to_smt(
+            "(ROOMCLEAN && (!INROOM) && (! DOORLOCKED))",
+            {
+                "ROOMCLEAN": TypedValuation("ROOMCLEAN", "bool", True),
+                "INROOM": TypedValuation("INROOM", "bool", True),
+                "DOORLOCKED": TypedValuation("DOORLOCKED", "bool", True)
+            },
+            True)
