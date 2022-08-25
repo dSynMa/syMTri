ad3647fa954577cc5b2095306df52ef182a8aeed
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2022-08-25 09:46:18 +0200

    refined synthesis workflow when unrealisable (up to model checking and producing a counterexample)

--- synthesis/synthesis.py
+++ synthesis/synthesis.py
@@ -1,15 +1,16 @@
-import re
 from typing import Tuple
 
 from programs.analysis.model_checker import ModelChecker
-from programs.analysis.util import create_nuxmv_model
+from programs.analysis.predicate_abstraction import predicate_abstraction, abstraction_to_ltl
+from programs.util import symbol_table_from_program, create_nuxmv_model_for_compatibility_checking
 from programs.program import Program
 from prop_lang.formula import Formula
 from prop_lang.parsing.string_to_ltl import string_to_ltl
-from prop_lang.util import tighten_ltl, conjunct, implies
+from prop_lang.util import implies
 from prop_lang.variable import Variable
 from strix import strix_adapter
 from strix.strix_adapter import syfco_ltl, syfco_ltl_in, syfco_ltl_out
+from synthesis.mealy_machine import MealyMachine
 
 
 def synthesize(aut: Program, ltl_text: str, tlsf_path: str, docker: bool) -> Tuple[bool, Program]:
@@ -21,7 +22,7 @@ def synthesize(aut: Program, ltl_text: str, tlsf_path: str, docker: bool) -> Tup
         out_acts = syfco_ltl_out(tlsf_path)
 
     ltl = string_to_ltl(ltl_text)
-    in_acts = [e for e in aut.env_events + aut.mon_events] + [e for e in aut.states]
+    in_acts = [e for e in aut.env_events + aut.out_events] + [e for e in aut.states]
     out_acts = [e for e in aut.con_events]
     # TODO validate in_acts, out_acts against those from TLSF
     return abstract_synthesis_loop(aut, ltl, in_acts, out_acts, docker)
@@ -37,16 +38,29 @@ def abstract_synthesis_loop(program: Program, ltl: Formula, in_acts: [Variable],
     (real, mm) = strix_adapter.strix(abstract_problem, in_acts, out_acts, docker)
 
     if real:
-        return mm
+        return (real, mm)
     else:
-        name, vars, define, init, trans = program.to_nuXmv_with_turns()
-        name_mm, vars_mm, define_mm, init_mm, trans_mm = mm.to_nuXmv_with_turns()
+        program_nuxmv_model = program.to_nuXmv_with_turns()
+        mon_events = program.out_events \
+                     + [Variable(s) for s in program.states] \
+                     + [p for s in abstract_monitor.states if s != abstract_monitor.initial_state for p in s[1]]
+        mealy = mm.to_nuXmv_with_turns(mon_events)
+        print(mm.to_dot())
 
-        system = create_nuxmv_model(name + "&&" + name_mm,
-                                    list(set(vars + vars_mm)),
-                                    define + define_mm,
-                                    init + init_mm,
-                                    trans + trans_mm)
+        system = create_nuxmv_model_for_compatibility_checking(program_nuxmv_model, mealy, mon_events)
+
+        print(system)
 
         model_checker = ModelChecker()
-        NotImplemented
+        # Sanity check
+        result, ce = model_checker.check(system, "F FALSE", 50)
+        assert not result
+
+        result, ce = model_checker.check(system, "G !mismatch", None)
+        if result:
+            # then the problem is unrealisable (i.e., the counterstrategy is a real counterstrategy)
+            return False, mm
+        else:
+            print(ce)
+            # TODO refinement of abstraction
+            raise NotImplementedError()
