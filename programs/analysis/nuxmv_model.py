ca19c11c25f8787a6735a06e0a386b32044c2c6c
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2022-08-25 09:34:21 +0200

    added nuxmv model class

---
+++ programs/analysis/nuxmv_model.py
@@ -0,0 +1,9 @@
+class NuXmvModel:
+
+    def __init__(self, name, vars, define, init, invar, trans):
+        self.name = name
+        self.vars = vars
+        self.define = define
+        self.init = init
+        self.invar = invar
+        self.trans = trans
