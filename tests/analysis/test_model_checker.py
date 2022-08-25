672ad5de79c2b785390cd107dc411e01b50a744d
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2022-08-25 09:27:44 +0200

    cleaned test, and using test in examples folder

--- tests/analysis/test_model_checker.py
+++ tests/analysis/test_model_checker.py
@@ -1,17 +1,17 @@
 from unittest import TestCase
 
 from programs.analysis.model_checker import ModelChecker
-from programs.analysis.util import create_nuxmv_model
+from programs.util import create_nuxmv_model
 from programs.parsing.string_to_program import string_to_program
 
 
 class TestModelChecker(TestCase):
     def test_check(self):
-        with open('../monitors/example.prog', 'r') as file:
+        with open('../../examples/parallel/arbiter/program.prog') as file:
             data = file.read()
             program = string_to_program(data)
             model_checker = ModelChecker()
-            nuxmv_model = create_nuxmv_model(*program.to_nuXmv_with_turns())
-            out = model_checker.check(nuxmv_model, "G FALSE")
+            nuxmv_model = create_nuxmv_model(program.to_nuXmv_with_turns())
+            out = model_checker.check(nuxmv_model, "F FALSE")
             print(out[1])
             assert(out[0] is False)
--- tests/monitors/test_program.py
+++ tests/monitors/test_program.py
@@ -6,8 +6,8 @@ from programs.parsing.string_to_program import string_to_program
 
 class MyTestCase(unittest.TestCase):
     def test_example(self):
-        with open('example.prog') as f:
-            lines = f.readlines()
+        with open('../../examples/parallel/arbiter/program.prog') as file:
+            lines = file.readlines()
             monitor = string_to_program("\n".join(lines))
             print(monitor)
             if monitor is None:
