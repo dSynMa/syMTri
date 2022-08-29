0c3bbcf5fb8ba395b3b3e7ca199f68925274f193
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2022-08-29 11:41:52 +0200

    if safety refinement fails, adding an extra step that allows A to talk about the inital values of the variables + small corrections to bugs like completing action sets and wrapping in Variable, and adding an extra variable index

--- programs/analysis/refinement.py
+++ programs/analysis/refinement.py
@@ -8,7 +8,7 @@ from prop_lang.util import conjunct, conjunct_formula_set, neg
 from prop_lang.variable import Variable
 
 
-def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table) -> [FNode]:
+def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table, program) -> [FNode]:
     logic = "QF_UFLRA"  # TODO what to put here?
 
     smt_checker = SMTChecker()
@@ -17,7 +17,7 @@ def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table) -> [FNode
     reset_vars = [BiOp(Variable(v + "_" + str(i)), ":=", Variable(v)) for v in symbol_table.keys() for i in range(0, len(prefix))]
 
     new_symbol_table = {}
-    for i in range(0, len(prefix)):
+    for i in range(0, len(prefix) + 1):
         new_symbol_table.update({key + "_" + str(i): value for key, value in symbol_table.items()})
 
     Cs = set()
@@ -27,15 +27,14 @@ def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table) -> [FNode
     B = conjunct(neg(prefix[len(prefix) - 1].condition).replace(ith_vars(len(prefix) - 1)), init_prop)
 
     for j in reversed(range(0, len(prefix))):
-
         # localize ith state
         path_formula_set_A = []
         for i in range(0, j + 1):
             # TODO maybe add known predicates
             path_formula_set_A += [prefix[i].condition.replace(ith_vars(i))] + \
-                                  [BiOp(act.left.name + "_" + str(i + 1),
+                                  [BiOp(Variable(act.left.name + "_" + str(i + 1)),
                                         "=",
-                                        act.right.replace(ith_vars(i))) for act in prefix[i].action]
+                                        act.right.replace(ith_vars(i))) for act in program.complete_action_set(prefix[i].action)]
 
         path_formula_A = conjunct_formula_set(path_formula_set_A)
 
@@ -43,9 +42,9 @@ def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table) -> [FNode
         for i in range(j + 1, len(prefix)):
             # TODO maybe add known predicates
             path_formula_set_B += [prefix[i].condition.replace(ith_vars(i))] + \
-                                  [BiOp(act.left.name + "_" + str(i + 1),
+                                  [BiOp(Variable(act.left.name + "_" + str(i + 1)),
                                         "=",
-                                        act.right.replace(ith_vars(i))) for act in prefix[i].action]
+                                        act.right.replace(ith_vars(i))) for act in program.complete_action_set(prefix[i].action)]
         path_formula_B = conjunct_formula_set(path_formula_set_B)
 
         Aj = And(*path_formula_A.to_smt(new_symbol_table))
@@ -58,8 +57,18 @@ def safety_abstraction(ce: [dict], prefix: [Transition], symbol_table) -> [FNode
         # if B is itself inconsistent
         if Cj.is_true():
             break
+        elif Cj.is_false():
+            Aj = And(*conjunct(init_prop, path_formula_A).to_smt(new_symbol_table))
+            Bj = And(*conjunct(B, path_formula_B).to_smt(new_symbol_table))
+            Cj = smt_checker.binary_interpolant(Aj, Bj, logic)
 
-        Cs |= {fnode_to_formula(Cj).replace(reset_vars)}
+            if Cj.is_true():
+                break
+            elif Cj.is_false():
+                break
+
+        Cs |= {fnode_to_formula(Cj).replace(reset_vars).simplify()}
+        Cs |= {neg(fnode_to_formula(Cj).replace(reset_vars)).simplify()}
 
     return Cs
 
