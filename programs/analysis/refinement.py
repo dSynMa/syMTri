a62c4efad81897379be973619a8e0a2800606656
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2022-08-30 10:05:23 +0200

    further refining of the refinement function, putting init_prop with A, negating B, adding the previous predicate to B, and only considering the next transition not triggering when constructing B, rather than the whole suffix

--- programs/analysis/refinement.py
+++ programs/analysis/refinement.py
@@ -4,7 +4,8 @@ from pysmt.shortcuts import And
 from programs.analysis.model_checker import ModelChecker
 from programs.analysis.smt_checker import SMTChecker
 from programs.transition import Transition
-from programs.util import ce_state_to_formula, fnode_to_formula
+from programs.util import ce_state_to_formula, fnode_to_formula, ground_formula_on_ce_state_with_index, \
+    project_ce_state_onto_ev
 from prop_lang.biop import BiOp
 from prop_lang.util import conjunct, conjunct_formula_set, neg, G, F, implies
 from prop_lang.variable import Variable
@@ -15,23 +16,35 @@ def safety_refinement(ce: [dict], prefix: [Transition], symbol_table, program) -
 
     smt_checker = SMTChecker()
 
+    # this will be used to add intermediate variables for each monitor state
     ith_vars = lambda i: [BiOp(Variable(v), ":=", Variable(v + "_" + str(i))) for v in symbol_table.keys()]
-    reset_vars = [BiOp(Variable(v + "_" + str(i)), ":=", Variable(v)) for v in symbol_table.keys() for i in range(0, len(prefix))]
 
+    # symbol table is updated with intermediate variables (plus last one, len(prefix), for state after last transition
     new_symbol_table = {}
     for i in range(0, len(prefix) + 1):
         new_symbol_table.update({key + "_" + str(i): value for key, value in symbol_table.items()})
 
+    # this will be used to generalise the interpolants references to intermediate variables to the original variable name
+    reset_vars = [BiOp(Variable(v + "_" + str(i)), ":=", Variable(v)) for v in symbol_table.keys() for i in range(0, len(prefix))]
+
+    # we collect interpolants in this set
     Cs = set()
 
+    # this characterises the variables at the initial state, we rename to name_0
     init_prop = ce_state_to_formula(ce[0], symbol_table).replace(ith_vars(0))
 
-    B = conjunct(neg(prefix[len(prefix) - 1].condition).replace(ith_vars(len(prefix) - 1)), init_prop)
+    # this characterise the base of the B of the interpolant problem, essentially, the initial proposition, and the
+    # negation of the condition of the transition the environment did not want to take
+    # B = conjunct(neg(prefix[len(prefix) - 1].condition).replace(ith_vars(len(prefix) - 1)), init_prop)
+
+    A = []
+    B = []
+    C = []
 
     for j in reversed(range(0, len(prefix))):
-        # localize ith state
+        # localize jth state
         path_formula_set_A = []
-        for i in range(0, j + 1):
+        for i in range(0, j):
             # TODO maybe add known predicates
             path_formula_set_A += [prefix[i].condition.replace(ith_vars(i))] + \
                                   [BiOp(Variable(act.left.name + "_" + str(i + 1)),
@@ -40,37 +53,38 @@ def safety_refinement(ce: [dict], prefix: [Transition], symbol_table, program) -
 
         path_formula_A = conjunct_formula_set(path_formula_set_A)
 
-        path_formula_set_B = []
-        for i in range(j + 1, len(prefix)):
-            # TODO maybe add known predicates
-            path_formula_set_B += [prefix[i].condition.replace(ith_vars(i))] + \
-                                  [BiOp(Variable(act.left.name + "_" + str(i + 1)),
+        projected_condition = prefix[j].condition.replace(ith_vars(j))
+        grounded_condition = ground_formula_on_ce_state_with_index(projected_condition, project_ce_state_onto_ev(ce[j], program.env_events + program.con_events), j)
+
+        path_formula_set_B = [neg(grounded_condition)]\
+                                   + \
+                                  [neg(BiOp(Variable(act.left.name + "_" + str(j + 1)),
                                         "=",
-                                        act.right.replace(ith_vars(i))) for act in program.complete_action_set(prefix[i].action)]
+                                        act.right.replace(ith_vars(j)))) for act in program.complete_action_set(prefix[j].action)]
         path_formula_B = conjunct_formula_set(path_formula_set_B)
 
-        Aj = And(*path_formula_A.to_smt(new_symbol_table))
-        Bj = And(*conjunct(B, path_formula_B).to_smt(new_symbol_table))
-        Cj = smt_checker.binary_interpolant(Aj, Bj, logic)
-        if Cj is None:
-            raise Exception("Interpolation being checked against formulas that are not contradictory: \n" +
-                            "A: " + str(Aj) +
-                            "\nB: " + str(Bj))
-        # if B is itself inconsistent
-        if Cj.is_true():
+
+
+        A = [And(*conjunct(init_prop, path_formula_A).to_smt(new_symbol_table))] + A
+        B = [And(*path_formula_B.to_smt(new_symbol_table))] + B
+        if len(C) > 0:
+            B[0] = And(B[0], C[0])
+
+        C = [smt_checker.binary_interpolant(A[0], B[0], logic)] + C
+        if C[0] is None:
+            print("I think that interpolation is being checked against formulas that are not contradictory: \n" +
+                            "A: " + str(A[0]) +
+                            "\nB: " + str(B[0]))
             break
-        elif Cj.is_false():
-            Aj = And(*conjunct(init_prop, path_formula_A).to_smt(new_symbol_table))
-            Bj = And(*conjunct(B, path_formula_B).to_smt(new_symbol_table))
-            Cj = smt_checker.binary_interpolant(Aj, Bj, logic)
+        # if B is itself inconsistent
+        if C[0].is_true():
+            break
+        elif C[0].is_false():
+            break
 
-            if Cj.is_true():
-                break
-            elif Cj.is_false():
-                break
-
-        Cs |= {fnode_to_formula(Cj).replace(reset_vars).simplify()}
-        Cs |= {neg(fnode_to_formula(Cj).replace(reset_vars)).simplify()}
+        Cj_generalised = ground_formula_on_ce_state_with_index(fnode_to_formula(C[0]), ce[0], 0).replace(reset_vars).simplify()
+        Cs |= {Cj_generalised}
+        Cs |= {neg(Cj_generalised).simplify()}
 
     return Cs
 
