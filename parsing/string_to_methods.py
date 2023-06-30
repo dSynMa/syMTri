479a1d69e36e50d7253b16d380b30421cde9c500
Luca Di Stefano <lucad@chalmers.se>
2023-06-30 16:01:23 +0200

    Cleanup test DSL code

--- parsing/string_to_methods.py
+++ parsing/string_to_methods.py
@@ -324,35 +324,37 @@ number::int = /[0-9]+/ ;
 
 test = """
 int cars_from_left := 0;
-    int cars_from_right := 0;
-    bool danger := false;
-    bool closed_from_left := true;
-    bool closed_from_right := true;
-    bool change_direction := false;
+int cars_from_right := 0;
+bool danger := false;
+bool closed_from_left := true;
+bool closed_from_right := true;
+bool change_direction := false;
 
-    method extern sensor_update(bool car_from_left_entry, bool car_from_right_entry,
-                                  bool car_from_left_exit, bool car_from_right_exit,
-                                  bool _change_direction){
-        assume(closed_from_left => !car_from_left_entry);
-        assume(closed_from_right => !car_from_right_entry);
+method extern sensor_update(bool car_from_left_entry, bool car_from_right_entry,
+                                bool car_from_left_exit, bool car_from_right_exit,
+                                bool _change_direction){
+    assume(closed_from_left => !car_from_left_entry);
+    assume(closed_from_right => !car_from_right_entry);
 
-        if (car_from_left_entry) cars_from_left++;
+    cars_from_left++;
 
-        if (car_from_right_entry) cars_from_right++;
+    if (car_from_left_entry) cars_from_left++;
 
-        if (car_from_left_exit) cars_from_left--;
+    if (car_from_right_entry) cars_from_right++;
 
-        if (car_from_right_exit) cars_from_right--;
+    if (car_from_left_exit) cars_from_left--;
 
-        change_direction := _change_direction;
+    if (car_from_right_exit) cars_from_right--;
 
-        danger := cars_from_left > 0 && cars_from_right > 0;
-    }
+    change_direction := _change_direction;
 
-    method intern control(bool close_from_left, bool close_from_right){
-        closed_from_left := close_from_left;
-        closed_from_right := close_from_right;
-    }
+    danger := cars_from_left > 0 && cars_from_right > 0;
+}
+
+method intern control(bool close_from_left, bool close_from_right){
+    closed_from_left := close_from_left;
+    closed_from_right := close_from_right;
+}
 """
 
 
