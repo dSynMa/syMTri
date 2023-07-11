b432b0fd108d716c72512278b201a73ada66843f
Luca Di Stefano <lucad@chalmers.se>
2023-07-11 15:50:31 +0200

    Add strix_tlsf.sh
    A helper script for syfco+strix

---
+++ scripts/strix_tlsf.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#parse command line arguments
+OPTIONS=()
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+if [[ $key = *.tlsf ]]; then
+    # tlsf file
+    TLSF=$1
+else
+    # option
+    OPTIONS+=("$1")
+fi
+# next argument
+shift
+
+done
+
+if [ -z $TLSF ]; then
+    echo "Error: No TLSF file given"
+    exit 1
+fi
+if [ ! -f $TLSF ]; then
+    echo "Error: File $TLSF does not exist"
+    exit 1
+fi
+
+ltlfile=$(mktemp) 
+INS=$(syfco -f ltl --print-input-signals $TLSF)
+OUTS=$(syfco -f ltl --print-output-signals $TLSF)
+LTL=$(syfco -f ltl -q double -m fully $TLSF -o $ltlfile)
+
+strix --ins "$INS" --outs "$OUTS" -F "$ltlfile" ${OPTIONS[@]}
+rm -f $ltlfile
